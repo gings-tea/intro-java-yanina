927ae171a5c406caab3b5c0b96da895027f5c492
Yanina <yanina.ovando@thoughtworks.com>
2018-09-03 14:05:51 -0300

    Added try catch
    Try and catch in case user enters strings instead of numbers

--- src/com/twu/intro/Main.java
+++ src/com/twu/intro/Main.java
@@ -1,47 +1,55 @@
 package com.twu.intro;
 
 import java.util.HashSet;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Main {
 
     public static void main (String[] args){
 
-        Scanner sc = new Scanner(System.in);
 
-        System.out.print("Please enter n: ");
-        int n = sc.nextInt();
+        try{
+            Scanner sc = new Scanner(System.in);
 
-        TriangleAndDiamondExercises triangleAndDiamondExercises = new TriangleAndDiamondExercises();
+            System.out.print("Please enter number: ");
+            int n = sc.nextInt();
 
-        System.out.println("\nEasiest exercise ever\n");
-        triangleAndDiamondExercises.horizontalLineOfAsteriskGivenN(1);
+            TriangleAndDiamondExercises triangleAndDiamondExercises = new TriangleAndDiamondExercises();
 
-        System.out.println("\nDraw a horizontal line\n");
-        triangleAndDiamondExercises.horizontalLineOfAsteriskGivenN(n);
+            System.out.println("\nEasiest exercise ever\n");
+            triangleAndDiamondExercises.horizontalLineOfAsteriskGivenN(1);
 
-        System.out.println("\nDraw a vertical line\n");
-        triangleAndDiamondExercises.verticalLineOfAsteriskGivenN(n);
+            System.out.println("\nDraw a horizontal line\n");
+            triangleAndDiamondExercises.horizontalLineOfAsteriskGivenN(n);
 
-        System.out.println("\nDraw a right triangle\n");
-        triangleAndDiamondExercises.triangleGivenN(n);
+            System.out.println("\nDraw a vertical line\n");
+            triangleAndDiamondExercises.verticalLineOfAsteriskGivenN(n);
 
-        System.out.println("\nIsosceles Triangle\n");
-        triangleAndDiamondExercises.isoscelesAsteriskTriangleGivenN(n);
+            System.out.println("\nDraw a right triangle\n");
+            triangleAndDiamondExercises.triangleGivenN(n);
 
-        System.out.println("\nDiamond\n");
-        triangleAndDiamondExercises.diamondAsteriskGivenN(n);
+            System.out.println("\nIsosceles Triangle\n");
+            triangleAndDiamondExercises.isoscelesAsteriskTriangleGivenN(n);
 
-        System.out.println("\nDiamond with Name\n");
-        triangleAndDiamondExercises.diamondAsteriskGivenNPlusName(n);
+            System.out.println("\nDiamond\n");
+            triangleAndDiamondExercises.diamondAsteriskGivenN(n);
 
-        System.out.println("\nFizzBuzz Exercise\n");
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        fizzBuzz.printFizzBuzzNumbers(100);
+            System.out.println("\nDiamond with Name\n");
+            triangleAndDiamondExercises.diamondAsteriskGivenNPlusName(n);
 
-        System.out.println("\nPrime Factors Exercise\n");
-        PrimeFactors primeFactors = new PrimeFactors();
-        System.out.println(primeFactors.primeFactorsSet(n));
+            System.out.println("\nFizzBuzz Exercise\n");
+            FizzBuzz fizzBuzz = new FizzBuzz();
+            fizzBuzz.printFizzBuzzNumbers(100);
+
+            System.out.println("\nPrime Factors Exercise\n");
+            PrimeFactors primeFactors = new PrimeFactors();
+            System.out.println(primeFactors.primeFactorsSet(n));
+
+        }
+        catch (InputMismatchException exception){
+            System.out.println("Error!!! Letters are not numbers!");
+        }
 
     }
 
